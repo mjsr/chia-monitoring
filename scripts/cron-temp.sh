668b795cfda0e19e3917d50df4d21fcc4cbdc851
Marcos R <marcos.ramos@gmail.com>
2021-05-05 22:10:10 -0700

    Add example to the temp script

--- scripts/cron-temp.sh
+++ scripts/cron-temp.sh
@@ -10,11 +10,15 @@ NOTIFIER="${CHIA_MONITORING_PATH}/venv/bin/python ${CHIA_MONITORING_PATH}/discor
 
 TEMPS="Storage temperature:"
 
+# Example line
+# Temperature:                        27 Celsius
 for d in "nvme0n1" "nvme1n1" "nvme2n1" "nvme3n1" "nvme4n1"; do
     TEMP=$(/usr/sbin/smartctl --all /dev/${d} | grep "^Temperature" | tr -s [:space:] | awk -v device="${d}" '{print device ": " $2}')
     TEMPS="$TEMPS\n$TEMP°C"
 done
 
+# Example line
+# 194 Temperature_Celsius     0x0022   032   041   000    Old_age   Always       -       32 (0 24 0 0 0)
 for d in "sda"; do
     TEMP=$(/usr/sbin/smartctl --all /dev/${d} | grep "Temperature_Celsius" | awk -v device="${d}" '{print device ": " $10}')
     TEMPS="$TEMPS\n$TEMP°C"
