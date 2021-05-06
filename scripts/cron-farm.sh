3853d92ab8e60abb5e2d354c1e30f8312670162f
Marcos R <marcos.ramos@gmail.com>
2021-05-06 12:32:13 -0700

    Change the default monitoring dir to test chiadog

--- scripts/cron-farm.sh
+++ scripts/cron-farm.sh
@@ -5,7 +5,7 @@
 # 30 * * * * /chia/chia-monitoring/scripts/cron-farm.sh >/dev/null 2>&1
 
 # Where you have this repo cloned
-CHIA_MONITORING_PATH=/chia/chia-monitoring
+CHIA_MONITORING_PATH=/chia/monitoring/chia-monitoring
 NOTIFIER="${CHIA_MONITORING_PATH}/venv/bin/python ${CHIA_MONITORING_PATH}/discord-notify.py --config ${CHIA_MONITORING_PATH}/secrets.yaml"
 
 # Where your chia installation is
